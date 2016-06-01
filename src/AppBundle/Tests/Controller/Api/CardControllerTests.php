1042d4a9f519993bedad28315aa812caa21bebb2
Martin Runge <martinrunge@posteo.de>
2016-06-01 12:29:18 +0200

    adjusted guzzle config

--- src/AppBundle/Tests/Controller/Api/CardControllerTests.php
+++ src/AppBundle/Tests/Controller/Api/CardControllerTests.php
@@ -8,9 +8,14 @@ class CardControllerTests extends \PHPUnit_Framework_TestCase
     // call via cli "vendor/bin/phpunit src/AppBundle/Tests/Controller/Api/CardControllerTests.php"
 
     public function testGet(){
-        $client = new Client();
+        $client = new Client([
+            'base_uri' => 'http://localhost:8000',
+            'default' => [
+                'exceptions' => false,
+            ]
+        ]);
 //        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'http://localhost:8000/api/cards');
+        $res = $client->request('GET', '/api/cards');
         echo $res->getStatusCode();
         echo "\n\n";
         echo $res->getHeaderLine('content-type');
