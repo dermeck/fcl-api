218b8f979734d4cb4e6c67291425ea4687121d13
Martin Runge <martinrunge@posteo.de>
2016-06-07 14:18:39 +0200

    test improvement: db is now filled with test data on setup and purged after test run

--- src/AppBundle/Test/ApiTestCase.php
+++ src/AppBundle/Test/ApiTestCase.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Test;
 
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class ApiTestCase extends \PHPUnit_Framework_TestCase
+class ApiTestCase extends KernelTestCase
 {
     private static $staticClient;
 
@@ -22,12 +24,35 @@ class ApiTestCase extends \PHPUnit_Framework_TestCase
                 'exceptions' => false,
             ]
         ]);
+
+        // make service container available
+        self::bootKernel();
     }
 
     public function setup()
     {
         // put client on non-static property
         $this->client = self::$staticClient;
+
+        $this->purgeDatabase();
     }
 
+    public function tearDown()
+    {
+        // do nothing, override parent function
+        // this prevent kernel shutdown
+    }
+
+    private function purgeDatabase()
+    {
+        $em = $this->getService('doctrine')->getManager();
+        $purger = new ORMPurger($em);
+//        $purger->setPurgeMode(2); // truncate => auto increment starts from 0
+        $purger->purge();
+    }
+
+    protected function getService($id)
+    {
+        return self::$kernel->getContainer()->get($id);
+    }
 }
--- src/AppBundle/Tests/Controller/Api/CardControllerTests.php
+++ src/AppBundle/Tests/Controller/Api/CardControllerTests.php
@@ -1,22 +1,55 @@
 <?php
 
 namespace AppBundle\Tests\Controller\Api;
+use AppBundle\Entity\Card;
+use AppBundle\Entity\Course;
 use AppBundle\Test\ApiTestCase;
-use GuzzleHttp\Client;
+use Doctrine\ORM\EntityManager;
+use Psr\Http\Message\ResponseInterface;
 
 class CardControllerTests extends ApiTestCase
 {
     // call via cli "vendor/bin/phpunit src/AppBundle/Tests/Controller/Api/CardControllerTests.php"
+    private $testCourseId;
+    private $testCardId;
+
+    public function setup()
+    {
+        parent::setup();
+
+        // setup test data
+        /** @var EntityManager $em */
+        $em = $this->getService('doctrine')->getManager();
+        $course = new Course();
+        $course->setName("TestCourse");
+        $em->persist($course);
+        $em->flush();
+        $this->testCourseId = $course->getId();
+
+        $card = new Card();
+        $card->setName('TestCard');
+        $card->setQuestion('question');
+        $card->setAnswer('answer');
+        $card->setCourse($course);
+        $card->setHint('hint');
+        $card->setPosition('0');
+        $em->persist($card);
+        $em->flush();
+
+        $this->testCardId = $card->getId();
+    }
 
     public function testGetSingle(){
-        $response = $this->client->get('/api/cards/1');
+        /** @var ResponseInterface $response */
+        $response = $this->client->get('/api/cards/' . $this->testCardId);
         echo $response->getBody();
-        
+
         $this->assertEquals(200, $response->getStatusCode());
         // valid json...
     }
 
     public function testGetCollection(){
+        /** @var ResponseInterface $response */
         $response = $this->client->get('/api/cards');
         echo $response->getBody();
 
@@ -26,13 +59,14 @@ class CardControllerTests extends ApiTestCase
 
     public function testPost(){
         $data = [
-            'name' => ' ',
+            'name' => 'test',
             'question' => 'question',
             'answer' => 'answer',
             'hint' => 'hint',
-            'course' => '2'
+            'course' => $this->testCourseId
         ];
-        
+
+        /** @var ResponseInterface $response */
         $response = $this->client->post('/api/cards', [
             'body' => json_encode($data)
         ]);
@@ -53,6 +87,6 @@ class CardControllerTests extends ApiTestCase
         // valid json...
         $finishedData = json_decode($response->getBody(), true);
         $this->assertArrayHasKey('question', $finishedData);
-
+        $this->assertEquals('test', $finishedData['name']);
     }
 }
